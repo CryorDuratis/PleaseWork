9329f6e9e0cd0f0fa79c87d2eae1b5ca8dca3deb
bryce <br0001ei@e.ntu.edu.sg>
2023-12-10 13:05:56 +0800

    fixed error: blank space in colgroup

--- src/component/sidebar.jsx
+++ src/component/sidebar.jsx
@@ -9,102 +9,89 @@ import Cookies from "js-cookie"
 import userServices from "../services/userServices.jsx"
 
 const Item = ({ title, to, icon, selected, setSelected }) => {
-   const navigate = useNavigate()
-   const primaryColor = "#556cd6" // Example primary color
-   const greyColor = "#999" // Example grey color
+  const navigate = useNavigate()
+  const primaryColor = "#556cd6" // Example primary color
+  const greyColor = "#999" // Example grey color
 
-   const handleNavigation = () => {
-      setSelected(title)
-      navigate(to)
-   }
+  const handleNavigation = () => {
+    setSelected(title)
+    navigate(to)
+  }
 
-   return (
-      <MenuItem active={selected === title} style={{ color: greyColor }} onClick={handleNavigation} icon={icon}>
-         <Typography>{title}</Typography>
-      </MenuItem>
-   )
+  return (
+    <MenuItem active={selected === title} style={{ color: greyColor }} onClick={handleNavigation} icon={icon}>
+      <Typography>{title}</Typography>
+    </MenuItem>
+  )
 }
 
 const Sidebarr = () => {
-   const [isCollapsed, setIsCollapsed] = useState(false)
-   const [selected, setSelected] = useState("Dashboard")
-   const [isAdmin, setIsAdmin] = useState(false)
-   const token = Cookies.get("token")
+  const [isCollapsed, setIsCollapsed] = useState(false)
+  const [selected, setSelected] = useState("Dashboard")
+  const [isAdmin, setIsAdmin] = useState(false)
+  const token = Cookies.get("token")
 
-   useEffect(() => {
-      async function checkAdmin() {
-         try {
-            await userServices.checkGroup("admin").then(function (result) {
-               if (result.response && result.response.status == 401) {
-                  Cookies.remove("token")
-                  Navigate("/")
-               }
-               if (result.result === true) {
-                  setIsAdmin(true)
-               }
-            })
-         } catch (e) {
-            setIsAdmin(false)
-         }
+  useEffect(() => {
+    async function checkAdmin() {
+      try {
+        await userServices.checkGroup("admin").then(function (result) {
+          console.log("result: ", result)
+          if (result.response && result.response.status == 401) {
+            Cookies.remove("token")
+            Navigate("/")
+          }
+          if (result.result === true) {
+            setIsAdmin(true)
+          }
+        })
+      } catch (e) {
+        setIsAdmin(false)
       }
-      checkAdmin()
-   }, [token])
+    }
+    checkAdmin()
+  }, [token])
 
-   //check state
-   //  useEffect(() => {
-   //     console.log("hi", isAdmin)
-   //  }, [isAdmin])
+  //check state
+  //  useEffect(() => {
+  //     console.log("hi", isAdmin)
+  //  }, [isAdmin])
 
-   return (
-      <Box
-         sx={{
-            ".css-ewdv3l": {
-               height: "100vh"
-            },
-            width: isCollapsed ? "80px" : "250px",
-            backgroundColor: "#556cd6"
-         }}
-      >
-         <Sidebar collapsed={isCollapsed}>
-            <Menu iconShape="square">
-               {/* LOGO AND MENU ICON */}
-               <MenuItem onClick={() => setIsCollapsed(!isCollapsed)} icon={isCollapsed ? <MenuOutlinedIcon /> : undefined}>
-                  {!isCollapsed && (
-                     <Box display="flex" justifyContent="space-between" alignItems="center">
-                        <Typography variant="h3">TMS</Typography>
-                        <IconButton onClick={() => setIsCollapsed(!isCollapsed)}>
-                           <MenuOutlinedIcon />
-                        </IconButton>
-                     </Box>
-                  )}
-               </MenuItem>
-               <Box paddingLeft={isCollapsed ? undefined : "10%"} sx={{ marginTop: "35px" }}>
-                  <Item
-                     title="Dashboard"
-                     to="/dashboard"
-                     icon={<HomeOutlinedIcon />}
-                     selected={selected}
-                     setSelected={setSelected}
-                  />
-                  {isAdmin ? (
-                     <Typography variant="h6" sx={{ m: "15px 0 5px 20px" }}>
-                        Admin Functions
-                     </Typography>
-                  ) : null}
-                  {isAdmin ? (
-                     <Item
-                        title="Admin"
-                        to="/admin"
-                        icon={<ContactsOutlinedIcon />}
-                        selected={selected}
-                        setSelected={setSelected}
-                     />
-                  ) : null}
-               </Box>
-            </Menu>
-         </Sidebar>
-      </Box>
-   )
+  return (
+    <Box
+      sx={{
+        ".css-ewdv3l": {
+          height: "100vh",
+        },
+        width: isCollapsed ? "80px" : "250px",
+        backgroundColor: "#556cd6",
+      }}
+    >
+      <Sidebar collapsed={isCollapsed}>
+        <Menu iconShape="square">
+          {/* LOGO AND MENU ICON */}
+          <MenuItem onClick={() => setIsCollapsed(!isCollapsed)} icon={isCollapsed ? <MenuOutlinedIcon /> : undefined}>
+            {!isCollapsed && (
+              <Box display="flex" justifyContent="space-between" alignItems="center">
+                <Typography variant="h3">TMS</Typography>
+                <IconButton onClick={() => setIsCollapsed(!isCollapsed)}>
+                  <MenuOutlinedIcon />
+                </IconButton>
+              </Box>
+            )}
+          </MenuItem>
+          <Box paddingLeft={isCollapsed ? undefined : "10%"} sx={{ marginTop: "35px" }}>
+            <Item title="Dashboard" to="/dashboard" icon={<HomeOutlinedIcon />} selected={selected} setSelected={setSelected} />
+            {isAdmin ? (
+              <Typography variant="h6" sx={{ m: "15px 0 5px 20px" }}>
+                Admin Functions
+              </Typography>
+            ) : null}
+            {isAdmin ? <Item title="Admin" to="/admin" icon={<ContactsOutlinedIcon />} selected={selected} setSelected={setSelected} /> : null}
+          </Box>
+        </Menu>
+      </Sidebar>
+    </Box>
+  )
 }
 
 export default Sidebarr
--- src/page/Login.jsx
+++ src/page/Login.jsx
@@ -5,76 +5,64 @@ import { useNavigate } from "react-router-dom"
 import { toast } from "react-toastify"
 
 const Login = () => {
-   const navigate = useNavigate()
+  const navigate = useNavigate()
 
-   const [username, setUsername] = useState("")
-   const [password, setPassword] = useState("")
-   const [error, setError] = useState("")
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+  const [error, setError] = useState("")
 
-   const [isLoggedIn, setIsLoggedIn] = useState(false)
-   const [userInfo, setUserInfo] = useState(null)
+  // const [isLoggedIn, setIsLoggedIn] = useState(false)
+  // const [userInfo, setUserInfo] = useState(null)
 
-   //Submit Form
-   const handleSubmit = async event => {
-      event.preventDefault()
-      try {
-         const userData = await authService.login(username, password)
-         setUserInfo(userData) //storing user data
-         setIsLoggedIn(true) //Updating login state
-         toast.success("Logged in Successfully")
-         navigate("/dashboard")
-      } catch (err) {
-         console.error("login error:", err)
-         setError("Invalid credentials")
-      }
-   }
-   return (
-      <Container maxWidth="sm">
-         <Box display="flex" flexDirection="column" alignItems="center" justifyContent="center" minHeight="75vh">
-            <Typography variant="h4" style={{ margin: "40px 0", textAlign: "center" }}>
-               Task Management System
-            </Typography>
-            {/* LoginForm component */}
-            <Paper style={{ padding: 20 }}>
-               <Typography variant="h6" textAlign="centre ">
+  //Submit Form
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+    try {
+      const userData = await authService.login(username, password)
+      // setUserInfo(userData) //storing user data
+      // setIsLoggedIn(true) //Updating login state
+      toast.success("Logged in Successfully")
+      navigate("/dashboard")
+    } catch (err) {
+      console.error("login error:", err)
+      setError("Invalid credentials")
+    }
+  }
+  return (
+    <Container maxWidth="sm">
+      <Box display="flex" flexDirection="column" alignItems="center" justifyContent="center" minHeight="75vh">
+        <Typography variant="h4" style={{ margin: "40px 0", textAlign: "center" }}>
+          Task Management System
+        </Typography>
+        {/* LoginForm component */}
+        <Paper style={{ padding: 20 }}>
+          <Typography variant="h6" textAlign="centre ">
+            Login
+          </Typography>
+          <form onSubmit={handleSubmit}>
+            <Grid container direction="column" spacing={2}>
+              <Grid item>
+                <TextField label="username" type="text" fullWidth value={username} onChange={(e) => setUsername(e.target.value)} />
+              </Grid>
+              <Grid item>
+                <TextField label="Password" type="password" fullWidth value={password} onChange={(e) => setPassword(e.target.value)} />
+              </Grid>
+              {error && (
+                <Grid item>
+                  <Typography color="error">{error}</Typography>
+                </Grid>
+              )}
+              <Grid item>
+                <Button type="submit" variant="contained" color="primary" fullWidth>
                   Login
-               </Typography>
-               <form onSubmit={handleSubmit}>
-                  <Grid container direction="column" spacing={2}>
-                     <Grid item>
-                        <TextField
-                           label="username"
-                           type="text"
-                           fullWidth
-                           value={username}
-                           onChange={e => setUsername(e.target.value)}
-                        />
-                     </Grid>
-                     <Grid item>
-                        <TextField
-                           label="Password"
-                           type="password"
-                           fullWidth
-                           value={password}
-                           onChange={e => setPassword(e.target.value)}
-                        />
-                     </Grid>
-                     {error && (
-                        <Grid item>
-                           <Typography color="error">{error}</Typography>
-                        </Grid>
-                     )}
-                     <Grid item>
-                        <Button type="submit" variant="contained" color="primary" fullWidth>
-                           Login
-                        </Button>
-                     </Grid>
-                  </Grid>
-               </form>
-            </Paper>
-         </Box>
-      </Container>
-   )
+                </Button>
+              </Grid>
+            </Grid>
+          </form>
+        </Paper>
+      </Box>
+    </Container>
+  )
 }
 
 export default Login
